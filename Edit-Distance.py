dd0380ac5bf160587cc7e093d21b5005623efb87
ItsAndrewC! <andew73885@gmail.com>
2024-09-02 14:52:43 -0400

    debuggin Edit-Distance

--- Edit-Distance.py
+++ Edit-Distance.py
@@ -1,9 +1,6 @@
 class EditDistance:
     def __init__(self) -> None:
-        pass
-
-    def test(self) -> None:
-        print("Hello")
+        print("Instantiated...")
 
     def minDistance(self, word1: str, word2: str) -> int:
         cols = len(word1)
@@ -12,16 +9,14 @@ class EditDistance:
         M = [[0 for col in range(cols)] for row in range(rows)]
         M[0][0] = 1
 
-        for A in rows:
-            for B in cols:
-                if word1[A] == word2[B] and self.__isValid(A, B): 
+        for A in range(rows-1):
+            for B in range(cols-1):
+                cW1, cW2 = word1[A], word2[B]
+                if cW1 == cW2: 
                     M[A][B] = M[A-1][B-1]
                 else:
-                    M[A][B] = self.__getMin(M, A, B)
+                    M[A][B] = self.__getMin(M, A, B) + 1
         return M[rows][cols]
-
-    def __isValid(self, A: int, B: int) -> bool:
-        return A > 0 and B > 0 
 
     def __getMin(self, M: list, A: int, B: int) -> int:
         if A-1 < 0:
@@ -31,3 +26,4 @@ class EditDistance:
         return min(M[A-1][B], M[A][B-1])
     
 ED = EditDistance()
+print(ED.minDistance("horse", "rose"))
