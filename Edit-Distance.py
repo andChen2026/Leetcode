05f98430c193417f309cd6178b7da150fe8f15e5
ItsAndrewC! <andew73885@gmail.com>
2024-09-02 15:20:38 -0400

    use a ternary operator instead

--- Edit-Distance.py
+++ Edit-Distance.py
@@ -3,27 +3,34 @@ class EditDistance:
         print("Instantiated...")
 
     def minDistance(self, word1: str, word2: str) -> int:
-        cols = len(word1)
-        rows = len(word2)
+        cols = len(word1) 
+        rows = len(word2) 
 
         M = [[0 for col in range(cols)] for row in range(rows)]
-        M[0][0] = 1
 
-        for A in range(rows-1):
-            for B in range(cols-1):
-                cW1, cW2 = word1[A], word2[B]
-                if cW1 == cW2: 
-                    M[A][B] = M[A-1][B-1]
-                else:
-                    M[A][B] = self.__getMin(M, A, B) + 1
+        for A in range(0, rows):
+            for B in range(0, cols):
+                cW1, cW2 = word1[B], word2[A]
+                M[A][B] = self.__getValS(M, A, B) if cW1 == cW2 else self.__getValD(M, A, B)
         return M[rows][cols]
 
-    def __getMin(self, M: list, A: int, B: int) -> int:
-        if A-1 < 0:
-            return M[0][B-1]
-        if B-1 < 0:
-            return M[A-1][0]
-        return min(M[A-1][B], M[A][B-1])
+    def __getValS(self, M: list, A: int, B: int) -> int:
+        if A == 0 and B == 0:
+            return 0
+        elif A == 0:
+            return M[A][B-1]
+        elif B == 0:
+            return M[A-1][B]
+        return M[A-1][B-1]
+    
+    def __getValD(self, M: list, A: int, B: int) -> int:
+        if A == 0 and B == 0:
+            return 1
+        elif A == 0:
+            return M[0][B-1] + 1
+        elif B == 0:
+            return M[A-1][0] + 1
+        return min(M[A-1][B], M[A][B-1]) + 1
     
 ED = EditDistance()
 print(ED.minDistance("horse", "rose"))
